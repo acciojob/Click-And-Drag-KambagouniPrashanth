5b4e9a0866b02910fd9dc8a025a281ba3f72a519
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-08-02 07:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Your code here.
 const itemElement=document.getElementById("item");
+itemElement.addEventListener("mousedown",(e)=>{
+    console.log("mousedown")
+
+})
+itemElement.addEventListener("mouseup",(e)=>{
+    console.log("mouseup")
+
+})
 itemElement.addEventListener("dragstart",(e)=>{
     console.log("dragstart")
 
